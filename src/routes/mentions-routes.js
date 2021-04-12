fd9fcad436c6f7bd4cf359026e0a4a197e3316ac
Decxus <samuell.marques1@gmail.com>
2021-04-12 09:06:28 -0300

    Project Upload

---
+++ src/routes/mentions-routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const {
+  check
+} = require('express-validator');
+const mentionsController = require('../controllers/mentions-controller');
+
+router.get('/', mentionsController.listMentions);
+
+router.post('/', [check('friend').isLength({
+    min: 5
+  }).withMessage("O nome precisa ter no minimo 5 caracteres."),
+  check('mention').isLength({
+    min: 20,
+    max: 300
+  }).withMessage("A menção precisa ter no mínimo 20 caracteres e no máximo 300.")
+], mentionsController.createMention);
+
+router.put('/:id', [check('friend').optional().isLength({
+  min: 5
+}).withMessage("O nome precisa ter no minimo 5 caracteres."), check('mention').optional().isLength({
+  min: 20,
+  max: 300
+}).withMessage("A menção precisa ter no mínimo 20 caracteres e no máximo 300.")], mentionsController.updateMention);
+
+router.delete('/:id', mentionsController.deleteMention);
+
+
+module.exports = router;
